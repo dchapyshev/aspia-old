66cd62994d2c6476b40ef745614f9ee03d6c60c2
lentind@gmail.com <lentind@gmail.com@05a0c0c9-f27a-ab1a-4ef4-f07a81c26cbd>
2011-07-02 08:45:43 +0000

    - Работа над сохранением файла-отчета в выбранный формат (часть 5 из Х)

--- trunk/aspia/report/text.c
+++ trunk/aspia/report/text.c
@@ -7,3 +7,77 @@
 
 #include "../main.h"
 
+static FILE *hReport = NULL;
+
+
+BOOL
+TextAppendStringToFile(LPWSTR lpszString)
+{
+    DWORD dwLen = SafeStrLen(lpszString);
+
+    if (!hReport) return FALSE;
+
+    fwrite(lpszString, sizeof(WCHAR), dwLen, hReport);
+
+    return TRUE;
+}
+
+BOOL
+TextCreateReport(LPWSTR lpszFile)
+{
+    if (_wfopen_s(&hReport, lpszFile, L"wt+,ccs=UTF-8") != 0)
+        return FALSE;
+
+    if (!hReport) return FALSE;
+
+    return TRUE;
+}
+
+VOID
+TextCloseReport(VOID)
+{
+    fclose(hReport);
+    hReport = NULL;
+}
+
+VOID
+TextWriteValueString(LPWSTR lpszString)
+{
+    WCHAR szText[MAX_STR_LEN];
+
+    StringCbPrintf(szText, sizeof(szText), L"%s ", lpszString);
+    TextAppendStringToFile(szText);
+}
+
+VOID
+TextWriteItemString(LPWSTR lpszString)
+{
+    WCHAR szText[MAX_STR_LEN];
+
+    StringCbPrintf(szText, sizeof(szText), L"\n%s ", lpszString);
+    TextAppendStringToFile(szText);
+}
+
+VOID
+TextTableTitle(LPWSTR lpszTitle)
+{
+    WCHAR szText[MAX_STR_LEN];
+
+    StringCbPrintf(szText, sizeof(szText), L"\n%s\n", lpszTitle);
+    TextAppendStringToFile(szText);
+}
+
+VOID
+TextEndTable(VOID)
+{
+    TextAppendStringToFile(L"\n");
+}
+
+VOID
+TextWriteColumnString(LPWSTR lpszString)
+{
+    WCHAR szText[MAX_STR_LEN];
+
+    StringCbPrintf(szText, sizeof(szText), L"%s ", lpszString);
+    TextAppendStringToFile(szText);
+}
